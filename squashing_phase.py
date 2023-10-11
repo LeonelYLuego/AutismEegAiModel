4877b85d57d0969b93c7e3752ed30773e70cf6d0
Lomlomm <vanessaserna60@gmail.com>
2023-10-11 12:34:01 -0600

    first version of entropy obtained MSE

--- squashing_phase.py
+++ squashing_phase.py
@@ -5,22 +5,30 @@ import pandas as pd
 # Cada fila representa una muestra y cada columna un canal 
 
 #Función para calcular la entropía de cada canal 
-def calculate_entropy(data): 
-    entropies = []
-    for channel_data in data.T:
-        channel_entropy = entropy(channel_data)
-        entropies.append(channel_entropy)
-    return entropies
+def calculate_entropy(data, time_scale): 
+    #falta mandar el rango de empiezar y terminar el ciclo 
+    multiplier = 1/time_scale
+    addition_of_x = 0
+
+    for x_data in data:
+        addition_of_x += x_data
+
+    entropy = multiplier * addition_of_x
+    
+    return entropy #se retorna un escalar
 
 #Calculamos entropias de diferentes escalas dividiendo los datos en segmentos y calculando la entropia de cada segmento 
 def calculate_multiscale_entropy(data, scale_factor): 
     num_samples, num_channels = data.shape
     entropies_multiscale = []
 
+    first_column = data.iloc[:, 0] # Primera columna
+    print(first_column)
+    #tenermos que mandar la primera columna
     for i in range(0, num_samples, scale_factor):
-        segment = data.iloc[i:i+scale_factor, :]
-        channel_entropies = calculate_entropy(segment)
-        entropies_multiscale.append(channel_entropies)
+        segment = first_column.iloc[i:i+scale_factor]
+        channel_entropy = calculate_entropy(segment, scale_factor)
+        entropies_multiscale.append(channel_entropy)
     print(entropies_multiscale)
 
     return np.array(entropies_multiscale)
@@ -28,6 +36,6 @@ def calculate_multiscale_entropy(data, scale_factor):
 #definicion del factor de escala 
 data = pd.read_csv('./data_combined.csv')
 dataframe = pd.DataFrame(data, columns=data.columns)
-scale_factor = 700
+scale_factor = 100
 entropies_multiscale = calculate_multiscale_entropy(dataframe, scale_factor)
 entropies_multiscale
