5875cbd4321982efb746f6c4824ae21a0cee9206
Lomlomm <vanessaserna60@gmail.com>
2023-10-12 18:03:50 -0600

    edit source to not generate additional csv

--- source.py
+++ source.py
@@ -36,7 +36,65 @@ print(data.describe())
 ## Standardize
 scaler = StandardScaler()
 data_scaled = pd.DataFrame(scaler.fit_transform(data), columns=data.columns)
-new_data = data_scaled.to_csv("data_combined.csv", sep=',', index=False)
+def calculate_entropy(data, time_scale): 
+    #falta mandar el rango de empiezar y terminar el ciclo 
+    multiplier = 1/time_scale
+    addition_of_x = 0
+
+    for x_data in data:
+        addition_of_x += x_data
+
+    entropy = multiplier * addition_of_x
+    
+    return entropy #se retorna un escalar
+
+def calculate_multiscale_entropy(channel, scale_factor, num_samples): 
+    entropies_multiscale = []
+    channel_info = {}
+
+    #Calculamos entropias de diferentes escalas dividiendo los datos en segmentos y calculando la entropia de cada segmento 
+    for i in range(0, num_samples, scale_factor):
+        segment = channel.iloc[i:i+scale_factor]
+        channel_entropy = calculate_entropy(segment, scale_factor)
+        entropies_multiscale.append(channel_entropy)
+
+    channel_info['name'] = channel.name #asignamos el noombre del canal 
+    channel_info['entropy'] = entropies_multiscale #agregamos su conjunto de entropias por segmento 
+
+    return channel_info
+
+'''
+get_channels separa por canales la data obtenida de data_scaled y calcula la entropia por segmentos de las series de tiempo de los canales. 
+
+Input: Dataframe
+Output: List of objects [{
+        'name': 'channel 1_alfa', 
+        'entropy': [list of float numbers]
+    }, 
+    {
+        'name': 'channel 2_alfa', 
+        'entropy': [list of float numbers]
+    }, 
+]
+
+
+'''
+def get_channels(data): 
+    scale_factor = 100
+    num_samples, num_channels = data.shape
+    entropies_multiscale = []
+
+    #recorremos los canales para calcular sus entropias 
+    for i in range(0, num_channels): 
+        column = data.iloc[:, i]
+        channel_entropy_object = calculate_multiscale_entropy(channel=column, scale_factor=scale_factor, num_samples=num_samples)
+        entropies_multiscale.append(channel_entropy_object)
+    
+    print(entropies_multiscale) # entropies multiscale es una lista de diccionarios
+    return entropies_multiscale
+
+# dataframe = pd.DataFrame(data, columns=data.columns)
+get_channels(data_scaled)
 
 data_scaled.plot()
 plt.show()
