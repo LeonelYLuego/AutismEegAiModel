11a8b9a4159e21a88452abc6633ddb38ae29e32f
Leonel Fernández <leonel-leonel-1@hotmail.com>
2023-10-16 07:27:57 -0600

    Some fixes

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 from .config import config_by_name
 db = SQLAlchemy()
@@ -7,6 +8,7 @@ db = SQLAlchemy()
 def create_app(config_name):
     app = Flask('AutismAI')
     app.config.from_object(config_by_name[config_name])
+    CORS(app)
     db.init_app(app)
 
     with app.app_context():
